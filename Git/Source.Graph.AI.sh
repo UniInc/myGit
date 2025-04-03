7e9b50ae5cb77a9d1ca75b8f3eaa5bd817e99314
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2025-04-04 01:34:25 +0800

    Update and rename my-user.sh to Source.Graph.AI.sh

---
+++ Git/Source.Graph.AI.sh
@@ -0,0 +1,1 @@
+git config --global user.name "Source.Graph" ; git config --global user.name "Source Graph A.I."
--- Git/my-user.sh
+++
@@ -1,2 +0,0 @@
-git config --global user.name "Source.Graph"
-git config --global user.name "Source Graph A.I."
