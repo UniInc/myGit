ec6cbfea01ee57e4086b9313a4601e89f5a858fd
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2025-04-18 16:53:00 -0500

    Update Source.Graph.AI.sh

--- Git/Source.Graph.AI.sh
+++ Git/Source.Graph.AI.sh
@@ -1,1 +1,1 @@
-git config --global user.name "Source.Graph" ; git config --global user.name "Source Graph A.I."
+git config --global user.email "Source.Graph@unisocial.net" ; git config --global user.name "Source Graph A.I."
